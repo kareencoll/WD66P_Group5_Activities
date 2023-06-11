61c253caed6e61b797eaefdc3ba76a9683dbbc90
Hanna Trisha B. Napa <trisha.napa@gmail.com>
2023-06-11 13:18:29 +0800

    WIP [Remaining stocks override] Activity 5 of Hanna Trisha Napa

--- napa_hannatrisha/activity5/index.php
+++ napa_hannatrisha/activity5/index.php
@@ -3,49 +3,56 @@
 $item1 = 100;
 $item2 = 200;
 $item3 = 300;
+$remainingstocks1 = 0;
+$remainingstocks2 = 0;
+$remainingstocks3 = 0;
 
 
-echo getTotalInventory(10, 1);
+echo getTotalInventory(1, 10);
 echo "\n";
-echo getTotalInventory(10, 1);
+echo getTotalInventory(2, 10);
 echo "\n";
-echo getTotalInventory(10, 1);
+echo getTotalInventory(1, 10);
+echo "\n";
+echo getTotalInventory(2, 10);
 
 
 
-function getTotalInventory($qty, $item){
-  
+
+function getTotalInventory($item, $qty)
+{
   
   if ($item == 1)
   {
     global $item1;
-    $TotalStocks = $item1 - $qty;
-    //return "Stocks: \nItem 1 $item1 stocks \nGet Item 1 ($qty) qty | Remaining Stocks = $TotalStocks \n";
-    return "Get Item 1 ($qty) qty | Remaining Stocks = $TotalStocks \n";
+    global $remainingstocks1;
+    $totalstocks1 = $item1 - $qty;
+    $remainingstocks1 -= $totalstocks1;
+    echo "Stocks: \nItem 1 $item1 stocks \nGet Item 1 ($qty) qty | Remaining Stocks = $remainingstocks1 \n";
   }
   
-  if ($item == 2)
+  else if ($item == 2)
   {
     global $item2;
-    $TotalStocks = $item2 - $qty;
-    //return "Stocks: \nItem 1 $item1 stocks \nGet Item 1 ($qty) qty | Remaining Stocks = $TotalStocks \n";
-    return "Get Item 2 ($qty) qty | Remaining Stocks = $TotalStocks \n";
+    global $remainingstocks2;
+    $totalstocks2 = $item2 - $qty;
+    $remainingstocks2 -= $totalstocks2;
+    echo "Stocks: \nItem 2 $item2 stocks \nGet Item 2 ($qty) qty | Remaining Stocks = $remainingstocks2 \n";
   }
   
-  if ($item == 3)
+  else if ($item == 2)
   {
-    global $item3;
-    $TotalStocks = $item2 - $qty;
-    //return "Stocks: \nItem 1 $item1 stocks \nGet Item 1 ($qty) qty | Remaining Stocks = $TotalStocks \n";
-    return "Get Item 3 ($qty) qty | Remaining Stocks = $TotalStocks \n";
+    global $item2;
+    global $remainingstocks2;
+    $totalstocks2 = $item2 - $qty;
+    $remainingstocks2 -= $totalstocks2;
+    echo "Stocks: \nItem 2 $item2 stocks \nGet Item 2 ($qty) qty | Remaining Stocks = $remainingstocks2 \n";
   }
   
   else{
     echo "Invalid stocks item";
   }
- 
-    
-    
+   
 }
   
 
