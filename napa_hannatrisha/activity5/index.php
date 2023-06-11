b1a742992d11a7f853236d64ede59dc54098fee6
Hanna Trisha B. Napa <trisha.napa@gmail.com>
2023-06-12 02:04:20 +0800

    Final Revise Activity 5 of Hanna Trisha Napa [All Done and ready to submit]

--- napa_hannatrisha/activity5/index.php
+++ napa_hannatrisha/activity5/index.php
@@ -26,20 +26,23 @@ function getTotalInventory($item, $qty)
   
   if ($item == 1)
   {
+    echo "Item 1 $item1 stocks \n";
     $item1 = $item1 - $qty;
-    echo "Item 1 $item1 stocks \nGet Item 1 ($qty) qty | Remaining Stocks = $item1 \n";
+    echo "Get Item 1 ($qty)qty | Remaining Stocks = $item1 \n";
   }
   
   else if ($item == 2)
   {
+    echo "Item 2 $item2 stocks \n";
     $item2 = $item2 - $qty;
-    echo "Item 2 $item2 stocks \nGet Item 2 ($qty) qty | Remaining Stocks = $item2 \n";
+    echo "Get Item 2 ($qty)qty | Remaining Stocks = $item2 \n";
   }
   
   else if ($item == 3)
   {
+    echo "Item 3 $item3 stocks \n";
     $item3 = $item3 - $qty;
-    echo "Item 3 $item3 stocks \nGet Item 3 ($qty) qty | Remaining Stocks = $item3 \n";
+    echo "Get Item 3 ($qty) qty | Remaining Stocks = $item3 \n";
   }
   
   else{
