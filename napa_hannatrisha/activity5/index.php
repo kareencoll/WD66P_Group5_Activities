2b4880a429aa0c9b1e70238ac9442e27deb63736
Hanna Trisha B. Napa <trisha.napa@gmail.com>
2023-06-11 23:17:56 +0800

    WIP [Retouch stocks override] Activity 5 of Hanna Trisha Napa

--- napa_hannatrisha/activity5/index.php
+++ napa_hannatrisha/activity5/index.php
@@ -3,54 +3,54 @@
 $item1 = 100;
 $item2 = 200;
 $item3 = 300;
-$remainingstocks1;
-$remainingstocks2;
-$remainingstocks3;
 
+echo "Stocks: \nItem 1 $item1 stocks \nItem 2 $item2 stocks \nItem 3 $item3 stocks \n";
+echo "\n";
+echo "\n";
 
-echo getTotalInventory(2, 10);
+echo getTotalInventory(1, 3);
 echo "\n";
-echo getTotalInventory(1, 10);
+echo getTotalInventory(1, 5);
 echo "\n";
-echo getTotalInventory(3, 10);
+echo getTotalInventory(2, 5);
+echo "\n";
+echo getTotalInventory(1, 50);
+echo "\n";
+echo getTotalInventory(3, 5);
 
 function getTotalInventory($item, $qty)
 {
   global $item1;
   global $item2;
   global $item3;
-  global $remainingstocks1;
-  global $remainingstocks2;
-  global $remainingstocks3;
   
   if ($item == 1)
   {
-    //if ($remainingstocks1 == $item1)
-      $remainingstocks1 = $item1 - $qty;
-      echo "Stocks: \nItem 1 $item1 stocks \nGet Item 1 ($qty) qty | Remaining Stocks = $remainingstocks1 \n";
+    $remainingstocks1 = $item1 - $qty;
+    echo "Item 1 $item1 stocks \nGet Item 1 ($qty) qty | Remaining Stocks = $remainingstocks1 \n";
+    $item1 = $remainingstocks1;
   }
-  
   
   else if ($item == 2)
   {
-    
     $remainingstocks2 = $item2 - $qty;
-    echo "Stocks: \nItem 2 $item2 stocks \nGet Item 2 ($qty) qty | Remaining Stocks = $remainingstocks2 \n";
+    echo "Item 2 $item2 stocks \nGet Item 2 ($qty) qty | Remaining Stocks = $remainingstocks2 \n";
+    $item2 = $remainingstocks2;
   }
   
   else if ($item == 3)
   {
-    
     $remainingstocks3 = $item3 - $qty;
-    echo "Stocks: \nItem 3 $item3 stocks \nGet Item 3 ($qty) qty | Remaining Stocks = $remainingstocks3 \n";
+    echo "Item 3 $item3 stocks \nGet Item 3 ($qty) qty | Remaining Stocks = $remainingstocks3 \n";
+    $item3 = $remainingstocks3;
   }
   
   else{
-    echo "Invalid stocks item";
+    echo "Invalid item.";
   }
    
 }
-  
+ 
 ?>
 
 
@@ -77,28 +77,3 @@ function getTotalInventory($item, $qty)
 
 
 
-
-
-
-
-
-
-
-
-  //while($allInTotal == ""){
-  //$allInTotal += $totalPrice;
-  //return "$allInTotal";
-  
-  
-//$i = 1;
-//$number = 6;
-//while ($i <= $number) {
-    // generate array with values from 1 to $i
-    //$array = range(1, $i);
-    // if there're more than 1 element in array - output sum
-    //if (count($array) > 1) {
-             // 1+2+... part                // sum of elements of array
-        //echo implode('+', $array) . ' = ' . array_sum($array) . "\n";
-    //}
-    //$i++;
-//}
