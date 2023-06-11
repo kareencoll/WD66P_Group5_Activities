6dfa0dc0b649f105a52c51b01604488f104d2d61
Hanna Trisha B. Napa <trisha.napa@gmail.com>
2023-06-12 02:54:50 +0800

    DONE Activity 5 of Hanna Trisha Napa [ready for merging]

--- napa_hannatrisha/activity5/index.php
+++ napa_hannatrisha/activity5/index.php
@@ -4,7 +4,7 @@ $item1 = 100;
 $item2 = 200;
 $item3 = 300;
 
-echo "Stocks: \nItem 1 = $item1 stocks \nItem 2 = $item2 stocks \nItem 3 = $item3 stocks \n";
+echo "Inventory List: \nItem 1 = $item1 stocks \nItem 2 = $item2 stocks \nItem 3 = $item3 stocks \n";
 echo "\n";
 echo "\n";
 
@@ -26,23 +26,23 @@ function getTotalInventory($item, $qty)
   
   if ($item == 1)
   {
-    echo "Item 1 $item1 stocks \n";
+    echo "Item 1: $item1 stocks available \n";
     $item1 = $item1 - $qty;
-    echo "Get Item 1 ($qty)qty | Remaining Stocks = $item1 \n";
+    echo "Get Item 1 - ($qty)qty | Remaining Stocks = $item1 \n";
   }
   
   else if ($item == 2)
   {
-    echo "Item 2 $item2 stocks \n";
+    echo "Item 2: $item2 stocks available \n";
     $item2 = $item2 - $qty;
-    echo "Get Item 2 ($qty)qty | Remaining Stocks = $item2 \n";
+    echo "Get Item 2 - ($qty)qty | Remaining Stocks = $item2 \n";
   }
   
   else if ($item == 3)
   {
-    echo "Item 3 $item3 stocks \n";
+    echo "Item 3: $item3 stocks available \n";
     $item3 = $item3 - $qty;
-    echo "Get Item 3 ($qty) qty | Remaining Stocks = $item3 \n";
+    echo "Get Item 3 - ($qty)qty | Remaining Stocks = $item3 \n";
   }
   
   else{
